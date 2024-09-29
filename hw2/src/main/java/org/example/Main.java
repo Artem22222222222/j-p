5a57110127c3ab8acee1fee2e37436a060d51285
Artem <artem.lyoshin@gmail.com>
2024-09-29 13:46:42 +0200

    v 1.1

--- hw2/src/main/java/org/example/Main.java
+++ hw2/src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        milesToKilometers(100);
+    }
+    public static void milesToKilometers(double miles){
+        System.out.println(miles*1.6);
     }
 }
